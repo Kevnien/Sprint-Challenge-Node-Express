6002709740562068b0f75cfaa31b7e4b9e1f53f8
Kevin Nguyen <kevinn858@gmail.com>
2018-10-05 09:41:56 -0700

    finished endpoints for projectModel

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -7,7 +7,43 @@ router.get('/', (req, res)=>{
         .then(projects =>{
             res.status(200).json(projects);
         })
-        .catch(err => console.error(err));
+        .catch(err => res.status(400).json(err));
+});
+
+router.get('/:id', (req, res)=>{
+    const {id} = req.params;
+    db.get(id)
+        .then(project =>{
+            res.status(200).json(project);
+        })
+        .catch(err => res.status(400).json(err));
+});
+
+router.post('/', (req, res)=>{
+    db.insert(req.body)
+        .then(project =>{
+            res.status(200).json(project);
+        })
+        .catch(err => res.status(400).json('ERROR' + project));
+});
+
+router.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    const project = req.body;
+    db.update(id, project)
+        .then( 
+            res.status(200).json(`Updated project with ID:${id}.`)
+        )
+        .catch( err => res.status(400).json(err));
+});
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params;
+    db.remove(id)
+        .then(
+            res.status(200).json(`Deleted project with ID:${id}.`)
+        )
+        .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
